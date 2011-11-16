7bfc3a2a031ce619e6bad9923bbce10581d4754b
Scott Johnson <7.scott.j@gmail.com>
2011-11-15 20:48:18 -0600

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bubibottle::Application.config.secret_token = 'aef6c2bad6312638f1f8a588e97f94a53ccc812b469d686e47b35a0a58b4c8a5f49bca394850a5e057f26bc1be19e6b4710ca7ce97d5c058f69e1e3d5d9683f3'
